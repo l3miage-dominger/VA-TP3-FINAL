a6657f56d0a32fa4378ecacd166d283167bf100f
dominger <ernesto.dominguez-montesino@etu.univ-grenoble-alpes.fr>
2024-04-07 16:40:39 +0200

    Avant boulot

---
+++ cr.md
@@ -0,0 +1,5 @@
+Mon binôme a arreté les études à cause d'un problème de santé
+juste avant le rendu du tp JPA. Ceci n'est peut être pas le meilleur rendu mais je crois que j'ai un peu compris au moins. Sachant que 
+j'ai travaillé 15 heures au restaurant entre vendredi, samedi et dimanche, j'ai tout essayé.
+
+Ernesto, plus de binôme.
--- server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import fr.uga.l3miage.spring.tp3.enums.TestCenterCode;
 import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
+import fr.uga.l3miage.spring.tp3.models.CandidateEvaluationGridEntity;
+import fr.uga.l3miage.spring.tp3.models.ExamEntity;
 import fr.uga.l3miage.spring.tp3.models.TestCenterEntity;
 import fr.uga.l3miage.spring.tp3.repositories.CandidateRepository;
 import fr.uga.l3miage.spring.tp3.repositories.TestCenterRepository;
@@ -89,15 +91,120 @@ public class CandidateRepositoryTest {
         candidateRepository.save(rogelio);
         candidateRepository.save(max);
 
+        //when
+
+        Set<CandidateEntity> candidatesDijon = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.DIJ);
+        Set<CandidateEntity> candidatesGrenoble = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.GRE);
+        Set<CandidateEntity> candidatesToulouse = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.TOU);
+
+
+
+        //then
+
+        assertThat(candidatesDijon).hasSize(2);
+        assertThat(candidatesGrenoble).hasSize(1);
+        assertThat(candidatesToulouse).hasSize(0);
+        //assertThat(candidatesGrenoble).containsExactlyInAnyOrder(ernesto);
+        //assertThat(candidatesDijon).containsExactlyInAnyOrder(rogelio, max);
+    };
+
+
+
+    @Test
+    void findAllByCandidateEvaluationGridEntitiesGradeLessThan(){  //Non encore fini
+
+
+        //given
+
+        ExamEntity exam = ExamEntity   //Le meme poids pour tous, ce n'est pas important ici
+                .builder()
+                .weight(1)
+                .build();
+
+        CandidateEvaluationGridEntity grid1rogelio = CandidateEvaluationGridEntity
+                .builder()
+                .grade(9.00)
+                .examEntity(exam)
+                .build();
+//
+//        CandidateEvaluationGridEntity grid2rogelio = CandidateEvaluationGridEntity
+//                .builder()
+//                .grade(9.00)
+//                .examEntity(exam)
+//                .build();
+
+        CandidateEvaluationGridEntity grid1ernesto = CandidateEvaluationGridEntity
+                .builder()
+                .grade(10.00)
+                .examEntity(exam)
+                .build();
+
+//        CandidateEvaluationGridEntity grid2ernesto = CandidateEvaluationGridEntity
+//                .builder()
+//                .grade(12.00)
+//                .examEntity(exam)
+//                .build();
+
+        CandidateEvaluationGridEntity grid1max = CandidateEvaluationGridEntity
+                .builder()
+                .grade(8.00)
+                .examEntity(exam)
+                .build();
+
+//        CandidateEvaluationGridEntity grid2max = CandidateEvaluationGridEntity
+//                .builder()
+//                .grade(15.00)
+//                .examEntity(exam)
+//                .build();
+
+        CandidateEntity ernesto = CandidateEntity
+                .builder()
+                .firstname("Ernesto")
+                .lastname("Dominguez")
+                .email("ernestodoinguez@outlook.com")
+                .phoneNumber("06168429")
+                .birthDate(LocalDate.of(2003,8,3))
+                .hasExtraTime(false)
+                .candidateEvaluationGridEntities(Set.of(grid1ernesto))
+                .build();
+        candidateRepository.save(ernesto);
+
+        System.out.println(ernesto.toString());
+
+        CandidateEntity rogelio = CandidateEntity
+                .builder()
+                .firstname("Rogelio")
+                .lastname("Alvarez")
+                .email("rogelioalvarez@outlook.com")
+                .phoneNumber("06152636")
+                .birthDate(LocalDate.of(2003,5,30))
+                .hasExtraTime(true)
+                .candidateEvaluationGridEntities(Set.of(grid1rogelio))
+                .build();
+        candidateRepository.save(rogelio);
+
+        CandidateEntity max = CandidateEntity
+                .builder()
+                .firstname("Maximiliano")
+                .lastname("Campos")
+                .email("maxcampos@outlook.com")
+                .phoneNumber("06152486")
+                .birthDate(LocalDate.of(2003,7,2))
+                .hasExtraTime(false)
+                .candidateEvaluationGridEntities(Set.of(grid1max))
+                .build();
+        candidateRepository.save(max);
 
 
         //when
 
-        Set<CandidateEntity> candidateEntityResponses = candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.DIJ);
+        Set<CandidateEntity> candidatesEnDessous = candidateRepository.findAllByCandidateEvaluationGridEntitiesGradeLessThan(10.0);
 
         //then
 
-        assertThat(candidateEntityResponses).hasSize(1);
+        assertThat(candidatesEnDessous).hasSize(2);
+
+
     };
 
 
